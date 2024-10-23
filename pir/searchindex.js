783e0de41e217a6c1b5bfc46528466d0844252a9
chrilves <christophe.calves@gmail.com>
2024-10-23 12:55:28 +0200

    Update

---
+++ pir/searchindex.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Cap'n Hugo Relearrrn Theme",
+    "uri": "/pir/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/pir/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "frontmatter",
+    "uri": "/pir/frontmatter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "options",
+    "uri": "/pir/options/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/pir/tags/index.html"
+  }
+]
